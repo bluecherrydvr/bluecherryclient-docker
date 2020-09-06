3cb83c4d2344dba423f9330b9e1d30de1d83f8f6
Curtis Hall <chall@corp.bluecherry.net>
2020-09-06 16:44:21 -0500

    Remove dangerous xhost flag and add volume for local recordings saved from the client

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-xhost +
+xhost +local:root
+
+# Create a directory for recordings saved from the Bluecherry client
+mkdir $HOME/bluecherry-recordings
 
 docker run -it \
    --net=host \
@@ -12,4 +15,6 @@ docker run -it \
    --device=/dev/snd/seq:/dev/snd/seq \
    --env="DISPLAY" --env="QT_X11_NO_MITSHM=1" --env="QT_GRAPHICSSYSTEM='native'" \
    --ipc host \
+   --user "$(id -u):$(id -g)" \
+   --volume="$HOME/bluecherry-recordings:/recordings" \
    --volume="$HOME/.Xauthority:/root/.Xauthority:rw" bluecherrydvr/bluecherry-client
